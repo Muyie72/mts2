8030d2f0990aa226dc27a7a15797f555be70bcea
Muyie72 <taiwo.muyiwa@outlook.com>
2020-07-29 22:20:54 -0700

    I want to see a new line

--- mts2.R
+++ mts2.R
@@ -3,3 +3,5 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub")
 
 print("PS: This is a revision, I forgot all I learnt the first time")
+
+print("Testing the version control now")
